83a88e67ac6df28d5c4edef9304881329088c866
aoifetiernan <32328448+aoifetiernan@users.noreply.github.com>
2017-10-15 11:13:12 +0100

    Add files via upload
    Initial test cases defined

---
+++ lowestCommonAncestorTest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class lowestCommonAncestorTest {
+
+
+	
+	//test it returns null for the LCA of an empty BT
+	@Test
+	public void testEmptyBT()
+	{
+		
+	}
+	
+	public void testOneNodeBT()
+	{
+		
+	}
+	
+	public void testTwoNodeBT()
+	{
+		
+	}
+
+}
+	
+
+
